ad1dbd7c9b021b9233c9d4634fef671a3a0f4fc5
jasimdipu <dipu.j247@gmail.com>
2021-09-15 20:10:46 +0600

    first class var, data type, conditions

---
+++ first_class_15_09_21/conditions.py
@@ -0,0 +1,54 @@
+# conditions
+
+# if shingara kahaoise = valo
+# else = chagol
+
+# ShamolKahoa = False
+
+# if ShamolKahoa == True:
+#     print("Valo")
+# else:
+#     print("kharap")
+
+# print("="*100)
+
+# # nested
+# age = int(input("Enter your age ="))
+
+# # type casting
+# # age = int(age)
+
+# # print(type(age))
+# # print(type(nid))
+
+# if age >= 18:
+#     print("Your age is ok, but do you have nid?")
+    
+#     nid = input("Do you have nid? ")
+
+#     if nid == "True":
+#         print("Ok done you can give your vote")
+#     else:
+#         print("you don't have your nid. bring it for your vote")
+# else:
+#     print("your are a teenager, you dont have permission for vote")
+
+
+# ladder
+# ==, <=, >= !=, or(|), and(&), not(!)
+cgpa = 2.5
+
+if cgpa >= 0 and cgpa < 1:
+    print("F")
+
+elif cgpa >= 1 and cgpa < 2:
+    print("D")
+
+elif cgpa >= 2 and cgpa < 3:
+    print("C")
+else:
+    print("A")
+
+
+
+
---
+++ first_class_15_09_21/variable_datatype.py
@@ -0,0 +1,52 @@
+# variables, data types, conditions, loops
+
+# variables => Storage -> data store using a word+number / warning-> do not use symbols
+"""
+12variable X
+@var X
+case sensative = > X, x are not same. They are differents
+"""
+name = "Chagol friend Shymol"
+Name = "Samir"
+# Single line comments
+
+"""
+multiline comments
+print(name)
+amr friend akta chagol tai likhlam
+"""
+
+name = "Valo Friend Shamol"
+Name = "Tuhin"
+Name2 = "Samir"
+print(name)
+print(Name)
+print(Name2)
+
+# data types
+# data types - > 1. primitive 2. non primitive
+# 1. primitive -> int, float, boolean
+# 2. non primitive -> String, Array
+
+int_var = 1000
+float_var = 10.50
+boolean_var = True
+boolean_var_2 = False
+
+
+string_var = "this is a string where we can declare @number like 1, 2, 3 and character like a, b, c"
+
+print(int_var)
+print(float_var)
+print(boolean_var)
+print(boolean_var_2)
+print(string_var)
+
+print(type(int_var))
+print(type(float_var))
+print(type(boolean_var))
+print(type(boolean_var_2))
+print(type(string_var))
+
+
+
